ab02809efff934daa6e0feb78ad9aace007de0ce
Vishnu Tripathi <vishnu.tripathi.2004@gmail.com>
2024-03-21 22:11:11 +0530

    put generate function form javascript into python code

--- Assignment_01/Vishnu Tripathi/app.py
+++ Assignment_01/Vishnu Tripathi/app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, request,  redirect, url_for
+from flask import Flask, jsonify, render_template, request,  redirect, url_for
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+import random
+import string
 
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 client = MongoClient('localhost', 27017)
 db = client.student_details
-
-
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
 
 @app.route("/")
 def form():
@@ -23,12 +19,17 @@ def data():
     data = {}
     if request.method == 'POST':
         data['Email'] = request.form['email']
-        data['Password'] = request.form['password']
-
         db.studentDetails.insert_one(data)
 
     return render_template('userGenerator.html')
 
+@app.route('/generate_username', methods=['GET'])
+def generate_username():
+    characters = string.ascii_lowercase
+    numbers = string.digits
+    word = ''.join(random.choice(characters) for _ in range(6))
+    word += ''.join(random.choice(numbers) for _ in range(2))
+    return jsonify({'username': word})
 
 if __name__ == '__main__':
     app.run(debug=True)
